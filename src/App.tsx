0d878e6b85a59dc2b8364888038d8761237b8dca
Musalu Wambua <musaluwambua@gmail.com>
2022-01-07 00:09:07 +0300

    removing side bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 //import {MovieObject} from './datatypes'
-import SideBar from './components/layout/SideBar';
 import Home from './components/pages/Home';
 import MovieDescription from './components/pages/MovieDescription';
 import { GlobalStyles } from './components/styles/Global.styles';
@@ -12,8 +11,6 @@ function App() {
   const MoviesObj = useFetchData("movies1");
   return (
     <div className="App">
-    
-    {/* <SideBar /> */}
     <Router>
       <Routes>
         <Route path ='/' element ={<Home moviesObj={MoviesObj} />} />
